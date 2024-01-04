1c4b001d8107e47886c1941a0c5268ef3d500aa9
raidon15 <benja.tsp@gmail.com>
2024-01-04 15:41:59 +0100

    add marker namespace for delete facilities

--- src/sphere_publish_service.cpp
+++ src/sphere_publish_service.cpp
@@ -25,18 +25,33 @@ rviz_visual_tools::RvizVisualToolsPtr visual_tools_;
 
 void sphere(const std::shared_ptr<visualization::srv::MarkerPublish::Request> request,
             std::shared_ptr<visualization::srv::MarkerPublish::Response> response)
-{ // Create pose
+{  // Create pose
+  visual_tools_->deleteMarker("next_goal",1);
   Eigen::Isometry3d pose;
-  pose = Eigen::AngleAxisd(M_PI / 4, Eigen::Vector3d::UnitY()); // rotate along X axis by 45 degrees
-  pose.translation() = Eigen::Vector3d(request->pose.position.x, request->pose.position.y, request->pose.position.z);          // translate x,y,z
+  pose = Eigen::AngleAxisd(M_PI / 4, Eigen::Vector3d::UnitY());  // rotate along X axis by 45 degrees
+  pose.translation() =
+      Eigen::Vector3d(request->pose.position.x, request->pose.position.y, request->pose.position.z);  // translate x,y,z
 
+  rviz_visual_tools::Colors color;
   // Publish arrow vector of pose
-  visual_tools_->publishSphere(pose, rviz_visual_tools::RED, rviz_visual_tools::XXLARGE);
+  if (request->color_value == 1)
+  {
+    color = rviz_visual_tools::RED;
+  }
+  else if (request->color_value == 2)
+  {
+    color = rviz_visual_tools::GREEN;
+  }
+  else
+  {
+    color = rviz_visual_tools::BLUE;
+  }
+  visual_tools_->publishSphere(pose, color, rviz_visual_tools::XXLARGE,request->marker_ns,1);
   // Don't forget to trigger the publisher!
   visual_tools_->trigger();
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
   std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("publish_marker_server");
